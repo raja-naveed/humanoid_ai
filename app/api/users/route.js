285e7763b75979e5096df4d98d25d1d350c42ae2
Sumit <sumit8444061@gmail.com>
2024-06-28 21:29:02 +0500

    Update route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,29 +1,58 @@
-import { createUserWithAccount,getUserByEmail } from "@/utils/user";
+// import { createUserWithAccount,getUserByEmail } from "@/utils/user";
+// import { NextResponse } from "next/server";
+// import bcrypt from "bcryptjs";
+
+// export const POST = async (req, res) => {
+//     try {
+//         const { name, email, password } = await req.json()
+//         const existingUser = await getUserByEmail(email)
+//         if (existingUser) {
+//           return NextResponse.json({message : "User already exists"}, { status: 400 });
+//         }
+//         const hashedPassword = await bcrypt.hash(password, 10);
+//         const newUser= await createUserWithAccount({
+//             name,
+//             email,
+//             password: hashedPassword,
+//             });
+
+//         return NextResponse.json({message : "User created successfully",
+//              data:{...newUser}}
+//              , { status: 201 });
+        
+
+//     } catch (error) {
+//       console.log(error)
+//       return NextResponse.json({message : "Error" , error:error.message}, { status: 500 });
+ 
+//     }
+// }
+
+import { createUserWithAccount, getUserByEmail } from "@/utils/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-export const POST = async (req, res) => {
-    try {
-        const { name, email, password } = await req.json()
-        const existingUser = await getUserByEmail(email)
-        if (existingUser) {
-          return NextResponse.json({message : "User already exists"}, { status: 400 });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser= await createUserWithAccount({
-            name,
-            email,
-            password: hashedPassword,
-            });
-
-        return NextResponse.json({message : "User created successfully",
-             data:{...newUser}}
-             , { status: 201 });
-        
-
-    } catch (error) {
-      console.log(error)
-      return NextResponse.json({message : "Error" , error:error.message}, { status: 500 });
- 
+export const POST = async (req) => {
+  try {
+    const { name, email, password } = await req.json();
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return NextResponse.json({ message: "User already exists" }, { status: 400 });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await createUserWithAccount({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json({
+      message: "User created successfully",
+      data: { ...newUser }
+    }, { status: 201 });
+
+  } catch (error) {
+    // console.error(error);
+    return NextResponse.json({ message: "Error", error: error.message }, { status: 500 });
+  }
 }
